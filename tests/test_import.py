74d3985ce1d8825984279500c8f3f946a4717b7c
ringsaturn <ringsaturn.me@gmail.com>
2024-10-29 21:00:54 +0800

    rm noqa

--- tests/test_import.py
+++ tests/test_import.py
@@ -16,18 +16,18 @@
 def test_import_viewport():
     from google.geo.type import (
         viewport_pb2 as google_dot_geo_dot_type_dot_viewport__pb2,
-    )  # noqa
+    )
 
     assert google_dot_geo_dot_type_dot_viewport__pb2.Viewport is not None
 
 
 def test_import_http():
-    from google.api import http_pb2 as google_dot_api_dot_http__pb2  # noqa
+    from google.api import http_pb2 as google_dot_api_dot_http__pb2
 
     assert google_dot_api_dot_http__pb2.Http is not None
 
 
 def test_import_latlng():
-    from google.type import latlng_pb2 as google_dot_type_dot_latlng__pb2  # noqa
+    from google.type import latlng_pb2 as google_dot_type_dot_latlng__pb2
 
     assert google_dot_type_dot_latlng__pb2.LatLng is not None
